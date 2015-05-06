ffdc01aab9b56581951286963055f4cea812814d
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2015-05-06 12:57:18 +0200

    Replaced pico_launch_files by pico_bringup

--- launch/multirobot/pico_rce_script.sh
+++ launch/multirobot/pico_rce_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PATH_TO_CONFIG=$HOME/ros/pico_launch_files/launch/multirobot/lo_client_pico.cfg
+PATH_TO_CONFIG=$HOME/ros/pico_bringup/launch/multirobot/lo_client_pico.cfg
 
 # Launch all pico nodes
-roslaunch pico_launch_files start.launch 
+roslaunch pico_bringup start.launch 
 
 # Configure interfaces and connections
 rce-ros $PATH_TO_CONFIG
